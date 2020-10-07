157bfe3b889cd5ac35a6c770c474fba9cf10fec7
Runyu Jin <rjin9@asu.edu>
2020-10-06 18:46:51 -0700

    updated based on the first pipeline

--- template/python-flow/index.py
+++ template/python-flow/index.py
@@ -9,7 +9,6 @@ from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
 from function import handler
 import os
 import time
-import cv2
 from datetime import datetime
 import constant
 import urllib3
@@ -18,15 +17,27 @@ import subprocess
 def invoke_next_function (bucket, files):
    next_function = os.getenv('NEXT_FUNCTION') 
    next_gateway = os.getenv('NEXT_GATEWAY') 
-   async_function = os.getenv('SYNC')
+   async_function = os.getenv('ASYNC')
    
    #print(next_function, next_gateway, async_function)
+   if next_function == '' or next_function is None:
+      print("no next function, return")
+      return
+   if next_gateway == '' or next_gateway is None:
+      print("no next gateway, return")
+      return
+   if async_function == '' or async_function is None:
+      async_function = '1'
+   if len(files) == 0:
+      return
    for file in files:      
       try:
+         print("invoking next function " + next_function)
          if async_function == '1':
-            e = 'echo ' + bucket + ' ' + file + ' | faas-cli invoke ' + next_function + ' -a --gateway ' + next_gateway
+            #curl http://192.168.0.200:8080/function/figlet --data "Hi"
+            e = 'curl -s http://' + next_gateway + '/async-function/' + next_function + ' --data "' + bucket + ' ' + file + '"'
          else:
-            e = 'echo ' + bucket + ' ' + file + ' | faas-cli invoke ' + next_function + ' --gateway ' + next_gateway
+            e = 'curl -s http://' + next_gateway + '/function/' + next_function + ' --data "' + bucket + ' ' + file + '"'
          subprocess.check_call(e, shell=True)
       except subprocess.CalledProcessError:
          pass # handle errors in the called executable
@@ -35,6 +46,8 @@ def invoke_next_function (bucket, files):
 
 def store_to_minio(bucket, ret):
    files = os.listdir(ret)
+   if len(files) == 0:
+      return
    #print(files)
    minioClient = Minio(constant.ENDPOINT,
                  access_key=constant.ACCESSKEY,
@@ -81,7 +94,7 @@ if __name__ == "__main__":
    file = file.rstrip("\n")
    new_file = load_from_minio(bucket, file)
    ret = handler.handle(new_file)
-   if ret != None:
+   if ret != None and ret != '':
      bucket, files = store_to_minio(bucket, ret)
      #print(bucket, files)
      invoke_next_function(bucket, files)
